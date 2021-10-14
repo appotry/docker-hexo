33dc6747a6e217d805d82271cf3021d5a6aa43a1
appotry <andycrusoe@gmail.com>
2021-10-14 08:59:17 +0800

    去除影响多主题插件

--- entrypoint.sh
+++ entrypoint.sh
@@ -26,14 +26,14 @@ else
     #cnpm install hexo-offline --save 
     #cnpm install hexo-web-push-notification --save 
     #cnpm install highlight.js --save 
-    echo "install live2d and model weier" 
+    #echo "install live2d and model weier" 
     #cnpm uninstall fsevents 
     #cnpm install fsevents --save 
-    echo "Please install browser plugin liveReload !" 
-    cnpm install livereload bufferutil utf-8-validate --save 
-    cnpm install hexo-helper-live2d --save 
-    cnpm install live2d-widget-model-lwet --save 
-    echo "install hexo-theme-matery" 
+    #echo "Please install browser plugin liveReload !" 
+    #cnpm install livereload bufferutil utf-8-validate --save 
+    #cnpm install hexo-helper-live2d --save 
+    #cnpm install live2d-widget-model-lwet --save 
+    #echo "install hexo-theme-matery" 
     #git clone https://github.com/blinkfox/hexo-theme-matery.git /app/themes/matery; 
 fi; 
 
